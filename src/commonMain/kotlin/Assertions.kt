ad559291824d73a047835ab9473e712dfaa42029
Tom Yuval <tomuvak@gmail.com>
2022-07-05 13:02:20 +0200

    Assertions (doc): add documentation to functions

--- src/commonMain/kotlin/Assertions.kt
+++ src/commonMain/kotlin/Assertions.kt
@@ -8,9 +8,19 @@ import kotlin.test.assertIs
 import kotlin.test.assertTrue
 import kotlin.test.fail
 
+/**
+ * Asserts that the given [optional] is [None].
+ */
 fun <T> assertNone(optional: Optional<T>) = assertEquals(None, optional)
 
+/**
+ * Asserts that the given [optional] is a [Value] and returns its underlying value.
+ */
 fun <T> assertValue(optional: Optional<T>): T = assertIs<Value<T>>(optional).value
+
+/**
+ * Asserts that the given [optional] is a [Value] and that its underlying value satisfies the given [predicate].
+ */
 fun <T> assertValue(optional: Optional<T>, predicate: (T) -> Boolean) = when(optional) {
     None -> fail("Expected Value satisfying predicate; got None")
     is Value -> assertTrue(
@@ -18,6 +28,10 @@ fun <T> assertValue(optional: Optional<T>, predicate: (T) -> Boolean) = when(opt
         "Expected Value satisfying predicate; got Value not satisfying predicate: ${optional.value}"
     )
 }
+
+/**
+ * Asserts that the given [optional] is a [Value] and that its underlying value is equal to the [expected] value.
+ */
 fun <T> assertValue(expected: T, optional: Optional<T>) = when (optional) {
     None -> fail("Expected Value of $expected; got None")
     is Value -> assertEquals(expected, optional.value)
